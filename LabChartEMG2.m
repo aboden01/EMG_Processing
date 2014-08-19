e04df25936edb7a2fc326bcd5fcb5983cf9daf51
Alexandra Boden <alexandra.boden@tufts.edu>
2014-08-19 14:53:19 -0400

    Consolidated functions
    Consolidated all EMG processing functions into the LabChartEMG2 script

--- LabChartEMG2.m
+++ LabChartEMG2.m
@@ -1,79 +1,107 @@
-%EMG difference L/R sides%
 
-%clear previous variables%
-clear i
-clear j
-clear LStart
-clear LEnd
-clear RStart
-clear REnd
-clear Difference
+choice = questdlg('Select a Process','EMG Processing Options','L/R EMG Overlap',...
+    'Duty Cycle w/ Video','Duty Cycle w/ EMG Only','default')
 
-%variables%
+if strcmp(choice,'L/R EMG Overlap')
+    %% EMG difference L/R sides
 
-LStart = input('Left EMG burston? ');
-LEnd = LeftEMG(:,3);
-RStart = input('Right EMG burston? ');
-REnd = RightEMG(:,3);
+    %clear previous variables%
+    clear i
+    clear j
+    clear LStart
+    clear LEnd
+    clear RStart
+    clear REnd
+    clear Difference
 
-% time of L/R EMG overlap%
-count = 1;
-for i = 1:length(LStart)
-    for j = 1:length(RStart)
-        if LStart(i) < RStart(j) && LEnd(i) > RStart(j)
-            Difference(count) = LEnd(i) - RStart(j);
-            count = count+1;
+    %variables%
+
+    LStart = input('Left EMG burston? ');
+    LEnd = LeftEMG(:,3);
+    RStart = input('Right EMG burston? ');
+    REnd = RightEMG(:,3);
+
+    %time of L/R EMG overlap%
+    count = 1;
+    for i = 1:length(LStart)
+        for j = 1:length(RStart)
+            if LStart(i) < RStart(j) && LEnd(i) > RStart(j)
+                Difference(count) = LEnd(i) - RStart(j);
+                count = count+1;
+            end
         end
     end
-end
 
-'Done.'
-
-
-%duty cycle%
-
-%clear previous variables%
-clear i
-clear j
-clear Trigger
-clear EMGStartRaw
-clear EMGStart
-clear EMGEndRaw
-clear EMGEnd
-clear TailBeatRaw
-clear TailRec
-clear TailBeat
-
-%variables%
-Trigger = input('Trigger vector?');
-EMGStartRaw = input('EMG burston? ');
-EMGEndRaw = input('EMG burstoff? ');
-EMGStart = EMGStartRaw - Trigger(1,1);
-EMGWidth = (EMGEndRaw(i)-EMGStartRaw(i))/1000
-TailBeatRaw = input('Tail beat cycle vector (peak times)?');
-TailFrames = input('Total number frames recorded?');
-FPS = input('Frames per second?');
-TailRec = TailFrames / FPS; %length of video recording in seconds%
-TailBeat = TailBeatRaw - TailRec;
-
-
-%ID tailbeat cycle associated with burst and calculate duty cycle%
-clear DutyCycle;
-count = 1;
-for i = 1:length(EMGStart)
-    for j = 1:length(TailBeat)-1
-        if EMGStart(i) > TailBeat(j) && EMGStart(i) < TailBeat(j+1)
-           DutyCycle(count)= EMGWidth(i)/abs(TailBeat(j+1)-TailBeat(j));
-            count = count+1;
-        end
-    end
-end
-
-'Done.'
-   
-
-Difference
+    'Done.'
+    
+    Difference
 average_Difference = mean(Difference)
 
-DutyCycle
-average_DutyCycle = mean(DutyCycle)
+elseif strcmp(choice,'Duty Cycle w/ Video')
+%% Duty Cycle
+
+    %clear previous variables%
+    clear i
+    clear j
+    clear Trigger
+    clear EMGStartRaw
+    clear EMGStart
+    clear EMGEndRaw
+    clear EMGEnd
+    clear TailBeatRaw
+    clear TailRec
+    clear TailBeat
+
+    %variables%
+    Trigger = input('Trigger vector?');
+    EMGStartRaw = input('EMG burston? ');
+    EMGEndRaw = input('EMG burstoff? ');
+    EMGStart = EMGStartRaw - Trigger(1,1);
+    EMGWidth = (EMGEndRaw(i)-EMGStartRaw(i))/1000
+    TailBeatRaw = input('Tail beat cycle vector (peak times)?');
+    TailFrames = input('Total number frames recorded?');
+    FPS = input('Frames per second?');
+    TailRec = TailFrames / FPS; %length of video recording in seconds%
+    TailBeat = TailBeatRaw - TailRec;
+
+
+    %ID tailbeat cycle associated with burst and calculate duty cycle%
+    clear DutyCycle;
+    count = 1;
+    for i = 1:length(EMGStart)
+        for j = 1:length(TailBeat)-1
+            if EMGStart(i) > TailBeat(j) && EMGStart(i) < TailBeat(j+1)
+               DutyCycle(count)= EMGWidth(i)/abs(TailBeat(j+1)-TailBeat(j));
+                count = count+1;
+            end
+        end
+    end
+
+    'Done.'
+
+
+    DutyCycle
+    average_DutyCycle = mean(DutyCycle)
+
+elseif strcmp(choice,'Duty Cycle w/ EMG Only')
+%% Duty Cycle w/ EMG Only
+
+    Trigger = input('Trigger = ' );
+    burston = input('EMG Onset = ');
+    burstoff = input('EMG Offset = ');   
+    EMGStart = burston - Trigger(1,2);
+    Width = burstoff(i)-burston(i)
+    EMGWidth = Width/1000;
+
+    clear DutyCycle
+    count = 1;
+    for i = 1:length(EMGStart)-1
+        if EMGStart(i) < 0
+            if EMGStart(i)-EMGStart(i+1)< 0.5
+                DutyCycle(count) = EMGWidth(i)/abs(EMGStart(i)-EMGStart(i+1));
+                count = count+1;
+            end
+        end
+    end
+end
+
