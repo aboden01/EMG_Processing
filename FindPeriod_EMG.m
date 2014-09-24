f762685adc3714ccc78878e59ea4c9630d94e43e
Alexandra Boden <alexandra.boden@tufts.edu>
2014-09-24 14:03:45 -0400

    Script to find best EMG period

---
+++ FindPeriod_EMG.m
@@ -0,0 +1,24 @@
+    clear Period_EMG
+
+    Trigger = input('Trigger = ' );
+    burston = input('EMG Onset = ');
+    burstoff = input('EMG Offset = ');   
+    EMGStart = burston - Trigger(1,2);
+    n = 1:length(burston);
+    EMGWidth = abs(burstoff(n)-burston(n)); 
+
+count = 1;
+    for i = 1:length(EMGStart)-1
+        if EMGStart(i) < 0
+            if EMGStart(i)-EMGStart(i+1)> -0.4
+                Period_EMG(count) = EMGStart(i)-EMGStart(i+1);
+                count = count+1;
+            end
+        end
+    end
+    
+   
+    median_Period = median(Period_EMG)
+    IQR_Period = iqr(Period_EMG);
+    sugg_Period = median_Period - IQR_Period
+   
